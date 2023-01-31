0b9831cf6ee3484258a866d924bfbff9714ae9c8
Serhii <s.saviuk@apiko.com>
2023-01-31 17:31:50 +0200

    add banner

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { Banner } from './common/components/banner/banner.component'
 import { Header } from './common/components/header/header.component'
 
 export const App = () => {
 	return (
 		<div>
 			<Header />
+			<Banner />
 		</div>
 	)
 }
